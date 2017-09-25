427e3caf3972f07eabfdf957e59ea1edc2aae1ee
Tom Vorel <tomvorel13@gmail.com>
2017-09-25 10:39:27 +0200

    Added tests for reducers, selectors and actions

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,48 @@
+import {
+  setStartDate,
+  setEndDate,
+  setTextFilter,
+  sortByDate,
+  sortByAmount
+} from "../../actions/filters";
+import moment from "moment";
+
+test("should generate set start date action object", () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    startDate: moment(0)
+  });
+});
+
+test("should generate set end date action object", () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    endDate: moment(0)
+  });
+});
+
+test("should generate set text filter object with text value", () => {
+  const action = setTextFilter("Something");
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: "Something"
+  });
+});
+
+test("should generate set test filter object with default", () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  });
+});
+
+test("should generate action object for sort by date", () => {
+  expect(sortByDate()).toEqual({ type: "SORT_BY_DATE" });
+});
+
+test("should generate action object for sort by amount", () => {
+  expect(sortByAmount()).toEqual({ type: "SORT_BY_AMOUNT" });
+});
