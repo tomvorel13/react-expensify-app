2039ce51439221df89558fe42cf4d8b1daec7b93
Tom Vorel <tomvorel13@gmail.com>
2017-09-27 07:19:08 +0200

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -14,20 +14,6 @@ import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 1000 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Water bill", amount: 6000 }));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter(""));
-}, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-console.log(store.getState());
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
