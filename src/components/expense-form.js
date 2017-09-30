f87f39a7daef1f7d4e613b5044abf76fba717c47
Tom Vorel <tomvorel13@gmail.com>
2017-09-30 12:25:29 +0200

    Setup test database env

--- src/components/expense-form.js
+++ src/components/expense-form.js
@@ -1,8 +1,7 @@
 import React from "react";
 import moment from "moment";
 import { SingleDatePicker } from "react-dates";
-const now = moment();
-console.log(now.format("MMM Do YYYY"));
+// const now = moment();
 
 class ExpenseForm extends React.Component {
   constructor(props) {
---
+++ src/playground/promises.js
@@ -0,0 +1,17 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Something went wrong!");
+  }, 3000);
+});
+
+console.log("before");
+
+promise
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+console.log("after");
