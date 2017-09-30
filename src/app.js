fc42d330b7299cb7e562410333940c51a1ba57b8
Tom Vorel <tomvorel13@gmail.com>
2017-09-30 14:24:39 +0200

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import "react-dates/lib/css/_datepicker.css";
 import "./firebase/firebase";
 
 //Actions
-import { addExpense } from "./actions/expenses";
+import { startSetExpenses } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
@@ -20,4 +20,9 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
-ReactDOM.render(jsx, document.getElementById("app"));
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById("app"));
+});
